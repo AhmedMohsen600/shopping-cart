0810cf1595fa4902993e1780f49fbdd96d69b954
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2022-08-17 18:16:56 +0200

    DONEE

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -1,27 +1,40 @@
-import { Stack } from "react-bootstrap";
-// import { useShoppingCart } from "../context/shppingCartContext";
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/shppingCartContext";
 import storeItems from "../data/items.json";
+import { formatCurrency } from "../util/formatCurrency";
 type CartItemProps = {
   id: number;
   qty: number;
 };
 export const CartItem = ({ id, qty }: CartItemProps) => {
-  //   const { removeFromCart } = useShoppingCart();
+  const { removeFromCart, totalPrice } = useShoppingCart();
   const item = storeItems.find((item) => item.id === id);
   if (item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         alt="koko"
-        style={{ width: "150px", height: "150px", objectFit: "cover" }}
+        style={{ width: "150px", height: "80px", objectFit: "cover" }}
       />
       <div className="me-auto">
         <div>
           {item.name} {qty > 1 && <span>x{qty}</span>}
         </div>
+        <div className="text-muted" style={{ fontSize: "20px" }}>
+          {formatCurrency(item.price)}
+        </div>
       </div>
+      <div>{formatCurrency(totalPrice(item.price))}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        // className="rounded"
+        onClick={() => removeFromCart(item.id)}
+      >
+        X
+      </Button>
     </Stack>
   );
 };
--- src/components/shoppingCart.tsx
+++ src/components/shoppingCart.tsx
@@ -1,10 +1,10 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/shppingCartContext";
 import { CartItem } from "./cartItem";
-
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../util/formatCurrency";
 export const ShoppingCart = () => {
   const { isOpen, closeCart, cartItems } = useShoppingCart();
-  console.log({ cartItems });
 
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
@@ -12,9 +12,20 @@ export const ShoppingCart = () => {
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} {...item} />
-        ))}
+        <Stack gap={4}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total:
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.qty;
+              }, 0)
+            )}
+          </div>
+        </Stack>
       </Offcanvas.Body>
     </Offcanvas>
   );
